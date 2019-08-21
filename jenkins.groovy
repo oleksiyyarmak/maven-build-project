a1b6da9054b94dd9810b2f8cdeafa1a8f2af4acb
Oleksii Yarmak <Oleksii_Yarmak@epam.com>
2019-08-21 16:35:28 +0300

    t

--- jenkins.groovy
+++ jenkins.groovy
@@ -24,7 +24,7 @@ node (label: 'master')
           stage ('parse') {
             def app_info = readFile(file: 'pom.xml')
             println "Trftff"
-            def xmlText = XmlParser.parseText(app_info)
+            def xmlText = new XmlParser().parseText(app_info)
             println "Bla"
             // println xmlText
 
