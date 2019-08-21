a7513821c4d79f7c43f110fc82e4ef7a18edecf8
Oleksii Yarmak <Oleksii_Yarmak@epam.com>
2019-08-21 16:46:29 +0300

    t

--- jenkins.groovy
+++ jenkins.groovy
@@ -26,7 +26,7 @@ node (label: 'master')
             println "Trftff"
             def xmlText = new XmlParser().parseText(app_info)
             println "Bla"
-            println "Name ${xmlText.properties.'legacy.version'}"
+            println "Name ${xmlText.'legacy.version'}"
 
 
 		}
