1d63695abb3c80c889cc12a09732f8abcd295ff7
oleksiyyarmak <50200271+oleksiyyarmak@users.noreply.github.com>
2019-08-21 16:17:33 +0300

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,40 +1,32 @@
-pipeline {
-    agent {
-    node {
-      label 'master'
-      customWorkspace "${JENKINS_HOME}/workspace/${JOB_NAME}/${BUILD_NUMBER}"
-    }
-         }
-    tools {
-        maven 'maven'
-        jdk 'JDK'
-    }
-       stages {
-        stage ('clone_git_repo') {
-            steps {
-                git credentialsId: 'oleksiyyarmak', url: 'https://github.com/oleksiyyarmak/maven-build-project.git'
-                sh "pwd"
-                sh "ls -la"
-            }
-        }
-       stage ('build_maven') {
-            steps {
-              dir("${JENKINS_HOME}/workspace/${JOB_NAME}/${BUILD_NUMBER}/gs-maven-master/initial") {
-                    sh "ls -la"
-                    sh 'mvn -Dmaven.test.failure.ignore=true install' 
-            		}
-					}
-		}	
-		stage ('tests') {
-			steps {
-                   dir("${JENKINS_HOME}/workspace/${JOB_NAME}/${BUILD_NUMBER}/gs-maven-master/test") {
-                    sh "pwd"
-                    sh "ls -la"
-                    sh 'sh run.sh' 
-                }
-            }
-                 
-        }  
-		}
- }
+#!/usr/bin/env groovy
 
+import hudson.model.*
+import hudson.EnvVars
+import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
+import org.apache.commons.io.FileUtils
+
+node (label: 'master')
+{
+          stage ('clone_git_repo') {
+
+            println 'Checkout Gitlab...'
+            checkout([$class: 'GitSCM', branches: [[name: "*/master"]],
+            doGenerateSubmoduleConfigurations: false,
+            extensions: [[$class: 'CleanBeforeCheckout']],
+            submoduleCfg: [],
+            userRemoteConfigs: [[credentialsId: 'oleksiyyarmak',
+            name: 'origin',
+            refspec: '',
+            url: 'https://github.com/oleksiyyarmak/maven-build-project.git']]
+            ])
+
+        }
+          stage ('parse') {
+            def app_info = readFile(file: 'pom.xml') 
+            def xmlText = new XmlSlurper().parse(app_info)
+            println xmlText
+            
+
+		}
+
+}
