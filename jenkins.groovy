e8f1589a35f8070e8be82bc649f246f5dbe038eb
Oleksii Yarmak <Oleksii_Yarmak@epam.com>
2019-08-22 16:30:05 +0300

    t

--- jenkins.groovy
+++ jenkins.groovy
@@ -40,7 +40,7 @@ node (label: 'master')
           println xmlText.properties['legacy.version']
           writeMavenPom(model: xmlText, file: 'pom.xml')
           println readFile(file: 'pom.xml')
-          sh "git commit -am "new version revision builded""
+          sh "git commit -am "version revision builded""
           sh "git push"
     }
 
