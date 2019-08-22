b2690cb19e8574798d622c69ed53b26c1ed07107
Oleksii Yarmak <Oleksii_Yarmak@epam.com>
2019-08-22 16:44:47 +0300

    t

--- jenkins.groovy
+++ jenkins.groovy
@@ -48,7 +48,7 @@ node (label: 'master')
           sh "git branch"
           sh "git add ."
           sh "git commit -m 'version revision builded'"
-          sh "git push -ur"
+          sh "git push -u"
     }
 
 }
