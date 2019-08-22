5dc97dc69deaaec462c9d4db3e3161554834d9f7
Oleksii Yarmak <Oleksii_Yarmak@epam.com>
2019-08-22 15:20:34 +0300

    t

--- jenkins.groovy
+++ jenkins.groovy
@@ -7,36 +7,39 @@ import org.apache.commons.io.FileUtils
 
 node (label: 'master')
 {
-          stage ('clone_git_repo') {
+  stage ('clone_git_repo')
+    {
 
-            println 'Checkout Gitlab...'
-            checkout([$class: 'GitSCM', branches: [[name: "*/master"]],
-            doGenerateSubmoduleConfigurations: false,
-            extensions: [[$class: 'CleanBeforeCheckout']],
-            submoduleCfg: [],
-            userRemoteConfigs: [[credentialsId: 'oleksiyyarmak',
-            name: 'origin',
-            refspec: '',
-            url: 'https://github.com/oleksiyyarmak/maven-build-project.git']]
-            ])
 
-        }
-          stage ('Parsing and modifying pom.xml') {
-            script{
-                      revision="2.1.${BUILD_NUMBER}-legacy-trit"
-                     }
-            println "Revision: ${revision}"
+          println 'Checkout Gitlab...'
+          checkout([$class: 'GitSCM', branches: [[name: "*/master"]],
+          doGenerateSubmoduleConfigurations: false,
+          extensions: [[$class: 'CleanBeforeCheckout']],
+          submoduleCfg: [],
+          userRemoteConfigs: [[credentialsId: 'oleksiyyarmak',
+          name: 'origin',
+          refspec: '',
+          url: 'https://github.com/oleksiyyarmak/maven-build-project.git']]
+          ])
 
-            def app_info = readFile(file: 'pom.xml')
-            println "Test1"
-            println app_info
-            def xmlText = readMavenPom(file: 'pom.xml')
-            println "Test2"
-            println xmlText.properties['legacy.version']
-            xmlText.properties['legacy.version'] = "${revision}".toString()
-            println xmlText.properties['legacy.version']
-            writeMavenPom(model: xmlText, file: 'pom.xml')
-            println readFile(file: 'pom.xml')
-          }
+    }
+  stage ('Parsing and modifying pom.xml')
+    {
+          script {
+                    revision="2.1.${BUILD_NUMBER}-legacy-trit"
+                 }
+          println "Revision: ${revision}"
+
+          def app_info = readFile(file: 'pom.xml')
+          println "Test1"
+          println app_info
+          def xmlText = readMavenPom(file: 'pom.xml')
+          println "Test2"
+          println xmlText.properties['legacy.version']
+          xmlText.properties['legacy.version'] = "${revision}".toString()
+          println xmlText.properties['legacy.version']
+          writeMavenPom(model: xmlText, file: 'pom.xml')
+          println readFile(file: 'pom.xml')
+    }
 
 }
