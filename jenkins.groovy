6103d23c7b85c30a0e3ce22f3da4aa94d780585b
oleksiyyarmak <50200271+oleksiyyarmak@users.noreply.github.com>
2019-09-16 19:06:20 +0300

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,4 +1,9 @@
 pipeline {
+    agent {
+    node {
+      label 'master'
+       }
+         }
 	stages {
 stage ('clone_git_repo')
     {
@@ -8,7 +13,7 @@ stage ('clone_git_repo')
             }
 			sidebarLinks {
             // use built-in image
-            link('https://jira.acme.org/', 'JIRA', 'notepad.png')
+            links('https://jira.acme.org/', 'JIRA', 'notepad.png')
             
         }
 	}
