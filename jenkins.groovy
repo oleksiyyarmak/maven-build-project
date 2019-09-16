6c0b5fee18e9b007b736aae173b837a25aa23454
oleksiyyarmak <50200271+oleksiyyarmak@users.noreply.github.com>
2019-09-16 18:58:59 +0300

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,9 +1,5 @@
-#!/usr/bin/env groovy
 
-import hudson.model.*
-import hudson.EnvVars
-import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
-import org.apache.commons.io.FileUtils
+pipeline {
 properties {
         sidebarLinks {
             // use built-in image
@@ -11,42 +7,11 @@ properties {
             
         }
     }
-node (label: 'master')
-{
-  stage ('clone_git_repo')
+stage ('clone_git_repo')
     {
+            script {
+            println "Hello"
+            }
 
 
-          println 'Checkout Gitlab...'
-          checkout([$class: 'GitSCM', branches: [[name: "*/master"]],
-          doGenerateSubmoduleConfigurations: false,
-          extensions: [[$class: 'CleanBeforeCheckout']],
-          submoduleCfg: [],
-          userRemoteConfigs: [[credentialsId: 'oleksiyyarmak',
-          name: 'origin',
-          refspec: '',
-          url: 'https://github.com/oleksiyyarmak/maven-build-project.git']]
-          ])
-
-    }
-  stage ('Parsing and modifying pom.xml')
-    {
-          def revision = ""
-          script {
-                    revision = "2.1.${BUILD_NUMBER}-legacy-trit"
-                 }
-          println "Revision: ${revision}"
-
-          // def app_info = readFile(file: 'pom.xml')
-          // println "Test1"
-          // println app_info
-          def xmlText = readMavenPom(file: 'pom.xml')
-          println "Test2"
-          println xmlText.properties['legacy.version']
-          xmlText.properties['legacy.version'] = revision.toString()
-          println xmlText.properties['legacy.version']
-          writeMavenPom(model: xmlText, file: 'pom.xml')
-          println readFile(file: 'pom.xml')
-
-    }
- }
+}
